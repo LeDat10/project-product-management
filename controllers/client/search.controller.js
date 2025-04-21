180c269b4a9431a50c8e8a992ff32e2b139ffef6
ledat <ledat1010204@gmail.com>
2025-04-21 16:31:27 +0700

    chỉnh sửa api search bên client

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -1,28 +1,24 @@
 const Product = require("../../models/product.model");
+const convertToSlugHelper = require("../../helper/convertToSlug");
 // [GET] /api/search
 module.exports.index = async (req, res) => {
     let product = [];
     try {
         const keyword = req.query.keyword;
         if (keyword) {
-            // Search
-            if (req.query.keyword) {
-                const keywordRegex = new RegExp(req.query.keyword, "i");
-                const stringSlug = convertToSlugHelper.convertToSlug(req.query.keyword);
-                const stringSlugRegex = new RegExp(stringSlug, "i");
-                find["$or"] = [
-                    { title: keywordRegex },
-                    { slug: stringSlugRegex }
-                ];
-            };
-            //End Search
+            const keywordRegex = new RegExp(keyword, "i");
+            const stringSlug = convertToSlugHelper.convertToSlug(keyword);
+            const stringSlugRegex = new RegExp(stringSlug, "i");
 
             product = await Product.find({
-                title: keywordRegex,
+                $or: [
+                    { title: keywordRegex },
+                    { slug: stringSlugRegex }
+                ],
                 deleted: false,
                 status: "active"
             }).select("title description price stock discountPercentage status thumbnail featured");
-            
+
             res.json({
                 code: 200,
                 message: "Tìm kiếm sản phẩm thành công!",
